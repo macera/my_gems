fa8857b2d01c317165602fd3ddd37a0f3a8b5b14
macera <ocak.kn@gmail.com>
2018-10-05 17:49:13 +0900

    add simplecov setting

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,11 @@
 require "bundler/setup"
 require "my_gems"
+require "simplecov"
+SimpleCov.start do
+  add_filter "/spec/"
+end
+
+
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
